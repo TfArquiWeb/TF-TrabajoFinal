1e312c566376f5f9a852496f0ff082c4adbf880f
TheDiegox980 <diego.05.pacheco.12@gmail.com>
2022-09-17 13:21:32 -0500

    Update empresario.ts

---
+++ Proyecto/src/app/model/empresario.ts
@@ -0,0 +1,13 @@
+export class Empresario{
+    idEmpresario:number=0;
+    nameEmpresario:string="";
+    apellidoEmpresario:string="";
+    emailEmpresario:string="";
+    dniEmpresario:number=0;
+    numeroEmpresario:number=0;
+    fotoEmpresario:string="";
+    razonsocialEmpresario:string="";
+    rucEmpresario:number=0;
+    rubroEmpresario:string="";
+    prefEmpresario:string="";
+}
