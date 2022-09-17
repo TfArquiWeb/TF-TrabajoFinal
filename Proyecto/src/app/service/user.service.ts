439ea7cbeae71077e241a1ecb90eddec8f13d068
TheDiegox980 <diego.05.pacheco.12@gmail.com>
2022-09-17 15:07:54 -0500

    Update user.service.ts

--- Proyecto/src/app/service/user.service.ts
+++ Proyecto/src/app/service/user.service.ts
@@ -1,4 +1,6 @@
+import { Empresario } from './../model/empresario';
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +8,10 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   url : string = 'http://localhost:5000/';
-  constructor() { }
+  constructor(private http:HttpClient) { }
   ListaContadores(){
+  }
+  getEmpresarios(){
+    return this.http.get<Empresario[]>(`${this.url}empresarios`)
   }
 }
