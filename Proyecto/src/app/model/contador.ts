a74bd5922e09023f22a8e4c256e950069cd162ab
TheDiegox980 <diego.05.pacheco.12@gmail.com>
2022-09-17 14:52:37 -0500

    a

--- Proyecto/src/app/model/contador.ts
+++ Proyecto/src/app/model/contador.ts
@@ -1,11 +1,11 @@
 import { Empresario } from "./empresario";
-export class Contador{
-    idContador:number=0;
-    nameContador:string="";
-    apellidoContador:string="";
-    emailContador:string="";
-    dniContador:number=0;
-    numeroContador:number=0;
-    fotoContador:string="";
-    Empresario:Empresario=new Empresario();
+export class Contador {
+    idContador: number = 0;
+    nameContador: string = "";
+    apellidoContador: string = "";
+    emailContador: string = "";
+    dniContador: number = 0;
+    numeroContador: number = 0;
+    fotoContador: string = "";
+    Empresario: Empresario = new Empresario();
 }
