32ec4e6285ed0a0c354de594b51cc52f4554cfae
RenzzoGutierrez20 <u202014777@upc.edu.pe>
2022-09-18 14:35:24 -0500

    change text

--- Proyecto/src/app/service/user.service.ts
+++ Proyecto/src/app/service/user.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
-
   url : string = 'http://localhost:5000/';
   constructor() { }
   ListaContadores(){
