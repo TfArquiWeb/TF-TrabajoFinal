13bad8ee46235fdf6e2382b931b90966e5ad75b1
LeonardoATO <93230012+LeonardoATO@users.noreply.github.com>
2022-09-18 06:03:25 -0500

    Listar empresario

--- Proyecto/src/app/app-routing.module.ts
+++ Proyecto/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { EmpresarioComponent } from './page/empresario/empresario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'empresarios', component: EmpresarioComponent, children:[
+
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
