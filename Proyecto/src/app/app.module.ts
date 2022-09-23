91b92cf02ab58f9ebd66c730832c43a8d5b5caa7
TheDiegox980 <diego.05.pacheco.12@gmail.com>
2022-09-22 22:59:12 -0500

    1

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { ExperienciaListarComponent } from './page/experiencia/experiencia-listar/experiencia-listar.component';
-import { Empresario } from 'src/app/model/empresario';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableModule} from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -72,7 +71,6 @@ import { CurriculumListarComponent } from './page/curriculum/curriculum-listar/c
     FormsModule,
     MatButtonModule,
     MatIconModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
