e4219e93352726cffc0e2980015a5f343b2ccddd
TheDiegox980 <83880331+TheDiegox980@users.noreply.github.com>
2022-11-07 09:27:09 -0500

    1

--- Proyecto/src/app/service/empresario.service.ts
+++ Proyecto/src/app/service/empresario.service.ts
@@ -9,17 +9,42 @@ import { Injectable } from '@angular/core';
 export class EmpresarioService {
   url: string = "http://localhost:8081/empresario"
   private listaCambio = new Subject<Empresario[]>()
+  private confirmaEliminacion = new Subject<Boolean>()
   constructor(private http: HttpClient) { }
-  listar() {
+  listarEmpresario() {
     return this.http.get<Empresario[]>(this.url);
-  }  
-  insertar(empresario: Empresario) {
+  }
+  insertarEmpresario(empresario: Empresario) {
+
     return this.http.post(this.url, empresario);
+  }
+
+  modificarEmpresario(empresario: Empresario) {
+
+    return this.http.put(this.url, empresario);
+  }
+  eliminarEmpresario(id: number) {
+
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  buscarEmpresario(texto: string) {
+
+    return this.http.post<Empresario[]>(`${this.url}/buscar`, texto);
+  }
+  listarIdEmpresario(id: number) {
+
+    return this.http.get<Empresario>(`${this.url}/${id}`);
+  }
+  getListaEmpresario() {
+    return this.listaCambio.asObservable();
   }
   setListaEmpresario(listaNueva: Empresario[]) {
     this.listaCambio.next(listaNueva);
   }
-  getListaEmpresario() {
-    return this.listaCambio.asObservable();
+  getConfirmaEliminacionEmpresario() {
+    return this.confirmaEliminacion.asObservable();
+  }
+  setConfirmaEliminacionEmpresario(estado: Boolean) {
+    this.confirmaEliminacion.next(estado);
   }
 }
